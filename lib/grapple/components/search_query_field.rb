8359ef4a447796a308dc2f2bf777c903509dd0c7
Eddie <epotocko@gmail.com>
2015-08-14 18:40:30 -0400

    Breaking up long line of code into multiple lines

--- lib/grapple/components/search_query_field.rb
+++ lib/grapple/components/search_query_field.rb
@@ -6,7 +6,11 @@ module Grapple
 			setting :search_query_field_class, 'search-query'
 			
 			def render(*options, &block)
-				template.text_field_tag(search_query_param.to_s, params[search_query_param.to_sym], { :class => search_query_field_class })
+				template.text_field_tag(
+					search_query_param.to_s, 
+					params[search_query_param.to_sym], 
+					{ :class => search_query_field_class }
+				)
 			end
 
 		end
