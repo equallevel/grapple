279c8f1977b79a0cdc32eabc806c69b7bc9f6a1f
mmaiatico <maiatico@gmail.com>
2015-08-17 14:22:26 -0400

    Search form component can now receive params to configure form path and options

--- lib/grapple/components/search_form.rb
+++ lib/grapple/components/search_form.rb
@@ -9,8 +9,12 @@ module Grapple
 			def render(*options, &block)
 				options = options[0] ? options[0] : {}
 				form_classes = [form_class]
+				form_path = options[:form_path] || {}
+				form_options = options[:form_options] || {}
+				form_options[:class] = form_classes.join(' ')
+
 				html = ''
-				html << template.form_tag({}, { :class => form_classes.join(' ') })
+				html << template.form_tag(form_path, form_options)
 				html << template.hidden_field_tag(page_param, 1)
 				# TODO: don't use tables for vertical alignment
 				html << '<table><tr>'
