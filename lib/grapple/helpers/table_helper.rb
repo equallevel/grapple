e4251bcb8d977a25aa3fe0ebd20d552d33825bd1
Eddie <epotocko@gmail.com>
2015-10-29 16:08:23 -0400

    Adding fix for params not being available in the table_helper.rb

--- lib/grapple/helpers/table_helper.rb
+++ lib/grapple/helpers/table_helper.rb
@@ -9,7 +9,10 @@ module Grapple
 				options = args[0] || {}
 				table_html_attributes = options[:html] || {}
 				builder_class = options[:builder] || @@builder
-				builder = builder_class.new(self, columns, records, params, options)
+				# params might not be defined (being called from a mailer)
+				# HACK: "defined? params" is returning method but when it gets called the method is not defined
+				request_params = params() rescue {}
+				builder = builder_class.new(self, columns, records, request_params, options)
 				output = capture(builder, &block)
 				(builder.before_table + builder.table(output, table_html_attributes) + builder.after_table).html_safe
 			end
